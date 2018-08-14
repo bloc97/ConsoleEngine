9504b09676e2c6dd8d6cbc97a8f243003cf0a54e
bloc97 <bloc97@users.noreply.github.com>
2018-08-13 20:32:59 -0400

    fix event

--- SpaceSurvival/src/spacesurvival/logic/Event.java
+++ SpaceSurvival/src/spacesurvival/logic/Event.java
@@ -386,7 +386,7 @@ public class Event {
                 "After building the promised shelters, confidence towards the leadership was resolved.\n"
                 ,
                 (c) -> {
-                    return c.checkEventChoosedId(40) && c.checkBuildingExists(Building.shelter, 2) && c.getDay() <= 25;
+                    return (c.checkEventChoosedId(40) && c.checkBuildingExists(Building.shelter, 2) && c.getDay() <= 25) || (c.checkEventChoosedId(42) && c.checkBuildingExists(Building.shelter, 3));
                 },
                 (c) -> {
                 },
