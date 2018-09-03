73c2c3374f098b626765d7e490eac36de311d72f
bloc97 <bloc97@users.noreply.github.com>
2018-09-03 14:53:48 -0400

    completed arbritrary painting with TextUtils

--- src/engine/ui/pixel/console/utils/TextUtils.java
+++ src/engine/ui/pixel/console/utils/TextUtils.java
@@ -7,8 +7,10 @@ package engine.ui.pixel.console.utils;
 
 import engine.ui.pixel.console.CharacterImage;
 import engine.ui.pixel.console.ConsoleFont;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,7 +27,7 @@ public interface TextUtils {
     }
     
     public static enum AlignType {
-        LEFT, CENTER, RIGHT, JUSTIFIED;
+        NONE, LEFT, CENTER, RIGHT, JUSTIFIED;
     }
     
     private static int getFromTruncatedArray(int i, int[] array) {
@@ -62,6 +64,30 @@ public interface TextUtils {
         return length;
     }
     
+    public static String preFormat(String string) {
+        final StringBuilder builder = new StringBuilder();
+        boolean isInModifier = false;
+        for (char c : string.toCharArray()) {
+            if (c == STARTCHAR && !isInModifier) {
+                isInModifier = true;
+                builder.append(c);
+                continue;
+            } else if (c == ENDCHAR && isInModifier) {
+                isInModifier = false;
+                builder.append(c);
+                continue;
+            } else if (c == STARTCHAR && isInModifier) {
+                continue;
+            } else if (c == ENDCHAR && !isInModifier) {
+                continue;
+            } else if (c == '\n' && isInModifier) {
+                continue;
+            }
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+    
     public static String wrapString(String string, WrapType wrapType, int lineWidth) {
         if (lineWidth <= 0) {
             return "";
@@ -72,7 +98,7 @@ public interface TextUtils {
         if (lineWidths.length <= 0) {
             return "";
         }
-        
+        string = preFormat(string);
         final StringBuilder builder = new StringBuilder();
         int charIndex = 0;
         int lineIndex = 0;
@@ -211,78 +237,184 @@ public interface TextUtils {
         }
     }
     
-    public static int drawString(CharacterImage image, String string, int x, int y, int defaultColor, WrapType wrapType, AlignType alignType, int leftPadding, int lineWidth) {
-        return drawString(image, new CharacterImage(), string, x, y, defaultColor, wrapType, alignType, leftPadding, lineWidth);
-    }
-    public static int drawString(CharacterImage image, CharacterImage altImage, String string, int x, int y, final int defaultColor, WrapType wrapType, AlignType alignType, int leftPadding, int lineWidth) {
-        String formattedString = wrapString(string, wrapType, (x > leftPadding) ? lineWidth - (x - leftPadding) : lineWidth, lineWidth);
+    public static class Cursor {
+        public final int defaultColor;
         
-        int currentARGB = defaultColor;
-        int currentUnderlineARGB = defaultColor;
-        boolean underline = false;
-        boolean strikethrough = false;
-        boolean forceAltColor = false;
+        public final int[] leftPadding;
+        public final int[] lineWidth;
         
-        if (x < leftPadding) {
-            x = leftPadding;
+        public volatile int currentARGB;
+        
+        public volatile boolean forceMaskColor;
+        public volatile int currentMaskARGB;
+        
+        public volatile boolean useMask;
+        public volatile char mask;
+        
+        public volatile AlignType alignType;
+        
+        public volatile int x;
+        public volatile int y;
+
+        public Cursor(int x, int y, int defaultColor, int leftPadding, int lineCharWidth) {
+            this(x, y, defaultColor, new int[] {leftPadding, leftPadding}, new int[] {lineCharWidth, lineCharWidth});
         }
         
-        for (String line : formattedString.split("\n")) {
-            int pad = 0;
-            switch (alignType) {
-                case CENTER:
-                    pad = (lineWidth - lengthWithoutModifier(line)) / 2;
-                    break;
-                case RIGHT:
-                    pad = (lineWidth - lengthWithoutModifier(line));
-                    break;
+        public Cursor(int x, int y, int defaultColor, int[] leftPadding, int[] lineCharWidth) {
+            this.defaultColor = defaultColor;
+            
+            if (leftPadding == null || leftPadding.length <= 0) {
+                leftPadding = new int[] {0};
             }
+            
+            if (lineCharWidth == null || lineCharWidth.length <= 0) {
+                lineCharWidth = new int[] {Integer.MAX_VALUE};
+            }
+            
+            this.leftPadding = leftPadding;
+            this.lineWidth = lineCharWidth;
+            if (x < getFromTruncatedArray(0, leftPadding)) {
+                x = getFromTruncatedArray(0, leftPadding);
+            }
+            this.x = x;
+            this.y = y;
+            currentARGB = defaultColor;
+            currentMaskARGB = defaultColor;
+            useMask = false;
+            mask = 0;
+            forceMaskColor = false;
+            alignType = AlignType.LEFT;
+        }
+        
+
+        public int getLeftPadding(int lineIndex) {
+            if (lineIndex == 0) {
+                return Math.max(getFromTruncatedArray(lineIndex, leftPadding), x);
+            }
+            return getFromTruncatedArray(lineIndex, leftPadding);
+        }
+        public int getLineWidth(int lineIndex) {
+            if (lineIndex == 0) {
+                return getFromTruncatedArray(lineIndex, lineWidth) - (x - getFromTruncatedArray(lineIndex, leftPadding));
+            }
+            return getFromTruncatedArray(lineIndex, lineWidth);
+        }
+        
+        
+        
+        public void parseModifier(String modifier) {
+            try {
+                //TODO: Check multiplier
+                if (modifier.equals("_")) { //Enable underline
+                    useMask = !useMask;
+                    mask = '_';
+                } else if (modifier.equals("-")) { //Enable/Disable strikethrough
+                    useMask = !useMask;
+                    mask = '─';
+                } else if (modifier.startsWith("^")) { //Enable/Disable mask
+                    useMask = !useMask;
+                    if (modifier.length() >= 2) {
+                        mask = modifier.charAt(1);
+                    }
+                } else if (modifier.startsWith("##")) { //Change underline color
+                    forceMaskColor = true;
+                    if (modifier.startsWith("+", 2)) { //Add ARGB color
+
+                    } else if (modifier.startsWith("*", 2)) { //Mult ARGB color
+
+                    } else if (modifier.startsWith("=", 2)) { //Set ARGB color
+
+                    } else { //Set HEX ARGB Color
+                        currentMaskARGB = Integer.parseUnsignedInt(modifier.substring(2), 16);
+                    }
+
+                } else if (modifier.startsWith("#")) { //Change text color
+                    if (modifier.startsWith("+", 2)) { //Add ARGB color
+
+                    } else if (modifier.startsWith("*", 2)) { //Mult ARGB color
+
+                    } else if (modifier.startsWith("=", 2)) { //Set ARGB color
+
+                    } else { //Set HEX ARGB Color
+                        currentARGB = Integer.parseUnsignedInt(modifier.substring(1), 16);
+                    }
+
+                } else if (modifier.isEmpty()) { //Reset
+                    currentARGB = defaultColor;
+                    currentMaskARGB = defaultColor;
+                    useMask = false;
+                    mask = 0;
+                    forceMaskColor = false;
+                } else if (modifier.equals("<")) {
+                    alignType = AlignType.LEFT;
+                } else if (modifier.equals("=")) {
+                    alignType = AlignType.CENTER;
+                } else if (modifier.equals(">")) {
+                    alignType = AlignType.RIGHT;
+                } else if (modifier.equals("!")) {
+                    alignType = AlignType.JUSTIFIED;
+                } else if (modifier.startsWith("&")) { //Set Alphacomposite
+                    
+                }
+
+            } catch (Exception ex) {
+            }
+        }
+    }
+    public static class PixelCursor extends Cursor {
+        public final ConsoleFont font;
+        
+
+        public PixelCursor(ConsoleFont font, int x, int y, int defaultColor, int leftPadding, int lineCharWidth) {
+            super(x, y, defaultColor, leftPadding, lineCharWidth);
+            this.font = font;
+        }
+
+        public PixelCursor(ConsoleFont font, int x, int y, int defaultColor, int[] leftPadding, int[] lineCharWidth) {
+            super(x, y, defaultColor, leftPadding, lineCharWidth);
+            this.font = font;
+        }
+    }
+    
+    public static int[] linePixelWidthToLineCharWidth(int[] linePixelWidth, int charPixelWidth) {
+        final int[] lineCharWidth = new int[linePixelWidth.length];
+        for (int i=0; i<lineCharWidth.length; i++) {
+            lineCharWidth[i] = linePixelWidth[i] / charPixelWidth;
+        }
+        return lineCharWidth;
+    }
+    
+    public static Cursor drawString(CharacterImage image, String string, WrapType wrapType, Cursor cursor) {
+        return drawString(image, null, string, wrapType, cursor);
+    }
+    
+    public static Cursor drawString(CharacterImage image, CharacterImage maskImage, String string, WrapType wrapType, Cursor cursor) {
+        if (maskImage == null) {
+            maskImage = new CharacterImage();
+        }
+        if (image == null) {
+            image = new CharacterImage();
+        }
+        
+        int[] lineCharWidth = new int[cursor.lineWidth.length + 1];
+        
+        for (int i=0; i<lineCharWidth.length; i++) {
+            lineCharWidth[i] = cursor.getLineWidth(i);
+        }
+        String formattedString = wrapString(string, wrapType, lineCharWidth);
+        
+        String[] lines = formattedString.split("\n");
+        int lineIndex = 0;
+        for (String line : lines) {
             int i = 0;
+            int currentLineWidth = cursor.getLineWidth(lineIndex);
+            int currentLinePadding = cursor.getLeftPadding(lineIndex);
             boolean isInModifier = false;
             String modifier = "";
             for (char c : line.toCharArray()) {
                 if (isInModifier) {
                     if (c == ENDCHAR) {
-                        try {
-                            //TODO: Check multiplier
-                            if (modifier.equals("_")) { //Enable/Disable underline
-                                underline = !underline;
-                            } else if (modifier.equals("-")) { //Enable/Disable underline
-                                strikethrough = !strikethrough;
-                            } else if (modifier.startsWith("##")) { //Change underline color
-                                forceAltColor = true;
-                                if (modifier.startsWith("+", 2)) { //Add ARGB color
-                                    
-                                } else if (modifier.startsWith("*", 2)) { //Mult ARGB color
-                                    
-                                } else if (modifier.startsWith("=", 2)) { //Set ARGB color
-                                    
-                                } else { //Set HEX ARGB Color
-                                    currentUnderlineARGB = Integer.parseUnsignedInt(modifier.substring(2), 16);
-                                }
-                                
-                            } else if (modifier.startsWith("#")) { //Change text color
-                                if (modifier.startsWith("+", 2)) { //Add ARGB color
-                                    
-                                } else if (modifier.startsWith("*", 2)) { //Mult ARGB color
-                                    
-                                } else if (modifier.startsWith("=", 2)) { //Set ARGB color
-                                    
-                                } else { //Set HEX ARGB Color
-                                    currentARGB = Integer.parseUnsignedInt(modifier.substring(1), 16);
-                                }
-                                
-                            } else if (modifier.isEmpty()) { //Reset
-                                currentARGB = defaultColor;
-                                currentUnderlineARGB = defaultColor;
-                                underline = false;
-                                strikethrough = false;
-                                forceAltColor = false;
-                            }
-                            
-                        } catch (Exception ex) {
-                        }
-                        
+                        cursor.parseModifier(modifier);
                         modifier = "";
                         isInModifier = false;
                     } else {
@@ -294,22 +426,119 @@ public interface TextUtils {
                     isInModifier = true;
                     continue;
                 }
-                image.setChar(x + pad + i, y, c);
-                image.setForegroundColor(x + pad + i, y, currentARGB);
-                if (underline) {
-                    altImage.setChar(x + pad + i, y, '_');
-                    altImage.setForegroundColor(x + pad + i, y, forceAltColor ? currentUnderlineARGB : currentARGB);
-                } else if (strikethrough) {
-                    altImage.setChar(x + pad + i, y, '─');
-                    altImage.setForegroundColor(x + pad + i, y, forceAltColor ? currentUnderlineARGB : currentARGB);
+                int pad = 0;
+                switch (cursor.alignType) {
+                    case CENTER:
+                        pad = (currentLineWidth - lengthWithoutModifier(line)) / 2;
+                        break;
+                    case RIGHT:
+                        pad = (currentLineWidth - lengthWithoutModifier(line));
+                        break;
+                }
+                cursor.x = currentLinePadding + pad + i;
+                image.setChar(cursor.x, cursor.y, c);
+                image.setForegroundColor(cursor.x, cursor.y, cursor.currentARGB);
+                if (cursor.useMask) {
+                    maskImage.setChar(cursor.x, cursor.y, cursor.mask);
+                    maskImage.setForegroundColor(cursor.x, cursor.y, cursor.forceMaskColor ? cursor.currentMaskARGB : cursor.currentARGB);
                 }
                 i++;
             }
-            x = leftPadding;
-            y++;
+            cursor.x++;
+            if (lineIndex < lines.length - 1) {
+                cursor.y++;
+                lineIndex++;
+                cursor.x = cursor.getLeftPadding(lineIndex);
+            }
         }
-        return y;
+        return cursor;
     }
+    
+    public static PixelCursor drawString(Graphics2D g2, String string, WrapType wrapType, PixelCursor cursor) {
+        if (g2 == null) {
+            return cursor;
+        }
+        
+        int[] linePixelWidth = new int[cursor.lineWidth.length + 1];
+        
+        for (int i=0; i<linePixelWidth.length; i++) {
+            linePixelWidth[i] = cursor.getLineWidth(i);
+        }
+        String formattedString = wrapString(string, wrapType, linePixelWidthToLineCharWidth(linePixelWidth, cursor.font.getWidth()));
+        
+        String[] lines = formattedString.split("\n");
+        int lineIndex = 0;
+        for (String line : lines) {
+            int i = 0;
+            int currentLineWidth = cursor.getLineWidth(lineIndex);
+            int currentLinePadding = cursor.getLeftPadding(lineIndex);
+            boolean isInModifier = false;
+            String modifier = "";
+            final char[] lineCharArray = line.toCharArray();
+            for (char c : lineCharArray) {
+                if (isInModifier) {
+                    if (c == ENDCHAR) {
+                        cursor.parseModifier(modifier);
+                        modifier = "";
+                        isInModifier = false;
+                    } else {
+                        modifier += c;
+                    }
+                    continue;
+                }
+                if (c == STARTCHAR) {
+                    isInModifier = true;
+                    continue;
+                }
+                int pad = 0;
+                switch (cursor.alignType) {
+                    case CENTER:
+                        pad = (currentLineWidth - (lengthWithoutModifier(line) * cursor.font.getWidth())) / 2;
+                        break;
+                    case RIGHT:
+                        pad = (currentLineWidth - (lengthWithoutModifier(line) * cursor.font.getWidth()));
+                        break;
+                }
+                cursor.x = currentLinePadding + pad + i * cursor.font.getWidth();
+                
+                paintChar(g2, new char[] {c}, 0, cursor.x, cursor.y, cursor.currentARGB, cursor.font);
+                
+                //image.setChar(cursor.x, cursor.y, c);
+                //image.setForegroundColor(cursor.x, cursor.y, cursor.currentARGB);
+                if (cursor.useMask) {
+                    paintChar(g2, new char[] {cursor.mask, cursor.mask, cursor.mask}, 1, cursor.x, cursor.y, cursor.forceMaskColor ? cursor.currentMaskARGB : cursor.currentARGB, cursor.font);
+                    //maskImage.setChar(cursor.x, cursor.y, cursor.mask);
+                    //maskImage.setForegroundColor(cursor.x, cursor.y, cursor.forceMaskColor ? cursor.currentMaskARGB : cursor.currentARGB);
+                }
+                i++;
+            }
+            cursor.x += cursor.font.getWidth();
+            if (lineIndex < lines.length - 1) {
+                cursor.y = cursor.y + cursor.font.getHeight();
+                lineIndex++;
+                cursor.x = cursor.getLeftPadding(lineIndex);
+            }
+        }
+        return cursor;
+    }
+    
+    private static void paintChar(Graphics2D g2, char[] chars, int index, int x, int y, int foregroundColor, ConsoleFont consoleFont) {
+        
+        g2.setColor(new Color(foregroundColor, true));
+
+        //final FontMetrics fontMetric = g2.getFontMetrics();
+
+        g2.drawChars(chars, index, 1, x, (consoleFont.getHeight() - consoleFont.getTopPadding()) + y);
+
+        if (chars[index] == '_' && !consoleFont.isUnderscoreContinuous()) {
+            if (consoleFont.isUnderscoreBreakOnLeft() && index > 0 && chars[index - 1] == '_') {
+                g2.fillRect(x, y + consoleFont.getUnderscoreYPos(), 1, 1);
+            } else if (index < chars.length - 1 && chars[index + 1] == '_') {
+                g2.fillRect(x + consoleFont.getWidth() - 1, y + consoleFont.getUnderscoreYPos(), 1, 1);
+            }
+        }
+    }
+    
     /*
     public static int drawString(Graphics2D g2, ConsoleFont font, String string, int x, int y, WrapType wrapType, AlignType alignType, int leftPaddingPixels, int lineWidthPixels) {
         
